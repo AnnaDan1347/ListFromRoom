669ffbc01bb04b0e4e1ca44dfb15d6e2b0337789
Anna Danilenko <ganna.danilenka@gmail.com>
2022-09-22 13:52:36 +0200

    add navigation

---
+++ app/src/main/java/by/annadanilenko/listfromroom/navigation/ScreenRouter.kt
@@ -0,0 +1,16 @@
+package by.annadanilenko.listfromroom.navigation
+
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.Navigation
+import by.annadanilenko.listfromroom.R
+
+class ScreenRouter {
+    companion object {
+//        fun openDetail(activity: FragmentActivity?) {
+//            activity?.let { fragmentActivity ->
+//                Navigation.findNavController(fragmentActivity, R.id.nav_host)
+//                    .navigate(R.id.action_introFragment_to_loginFragment)
+//            }
+//        }
+    }
+}
